513aaabd98e02f73d4ac292a88ed677b5d0a12b3
Daniel Cohen <94773442+DanielCohen002@users.noreply.github.com>
2021-12-09 18:23:04 -0500

    Add files via upload

---
+++ Chess/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace New_Chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IO.startGame();
+            int repeatPlaceHolder = 255;
+            while (repeatPlaceHolder == 255)
+            {
+                IO.movePiece();
+            }
+        }
+    }
+}
